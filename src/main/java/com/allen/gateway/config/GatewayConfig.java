233b9b9c5c74ad70e83421d9ca98a152c14cec13
allen <allen@localhost>
2020-12-16 16:40:48 +0800

    添加sentinel支持

--- src/main/java/com/allen/gateway/config/GatewayConfig.java
+++ src/main/java/com/allen/gateway/config/GatewayConfig.java
@@ -1,7 +1,21 @@
 package com.allen.gateway.config;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
 
 /**
  * 网关配置类
@@ -10,12 +24,35 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
  * @date Jul 2, 2020
  * @since 1.0.0
  */
-// @Configuration
+@Configuration
 public class GatewayConfig {
-	
-//	@Bean
+
+	private final List<ViewResolver> viewResolvers;
+
+	private final ServerCodecConfigurer serverCodecConfigurer;
+
+	public GatewayConfig(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+			ServerCodecConfigurer serverCodecConfigurer) {
+		this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+		this.serverCodecConfigurer = serverCodecConfigurer;
+	}
+
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+		// Register the block exception handler for Spring Cloud Gateway.
+		return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+	}
+
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	public GlobalFilter sentinelGatewayFilter() {
+		return new SentinelGatewayFilter();
+	}
+
+	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes().route(r -> r.path("/spring-cloud").uri("http://www.ityouknow.com").id("message_route"))
+		return builder.routes().route("message_test", r -> r.path("/spring-cloud").uri("http://www.ityouknow.com"))
 				.build();
 	}
 
